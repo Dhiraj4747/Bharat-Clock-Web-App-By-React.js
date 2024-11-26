aef4ecfefc8c02002794406ffb5ec70dc5aa4803
Dhiraj Patil <dhirajpatil1353@gmail.com>
2024-11-26 07:14:43 +0530

    changes

--- src/components/ClockTime.jsx
+++ src/components/ClockTime.jsx
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
+
 function ClockTime(){
-   let time = new Date();
+   const [time,setTime] = useState(new Date())
+   useEffect(()=>{
+      const intervalTd = setInterval(()=>{
+         setTime(new Date())
+      },1000)
+
+      return ()=>{
+         clearInterval(intervalTd)
+      }
+   },[])
    return<>
    <p className="lead">This is the current time :{time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
    </>
